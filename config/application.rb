c6997c01a7aa57b2266d0796e3ca61d4420f2f87
medavidchou <david50915@gmail.com>
2014-12-21 13:47:12 -0800

    gs

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Overwritten
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile = %w( *.js *.css )
+
+    config.assets.initialize_on_precompile = false
   end
 end
